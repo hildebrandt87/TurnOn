4bed1edf0b122eb8ef1919e778b1fda6b3b6c77d
hildebrandt87 <hildebrandt.kai87@googlemail.com>
2016-01-03 23:15:08 +0100

    Finale Version
    mit WinForm

---
+++ CWol.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TurnOn
+{
+    public class CWol
+    {
+        //*Übergabe des MAC Adresse als byte Array
+        public void WakeOnLan(byte[] mac)
+        {
+
+            //Gesendet an Broadcast 255.255.255.0  Port:40000
+            UdpClient Client = new UdpClient();
+            Client.Connect(IPAddress.Broadcast, 40000);
+
+            // nochmals prüfen
+            byte[] packet = new byte[17 * 6];
+
+            //byte array füllen
+            for (int i = 0; i < 6; i++)
+            {
+                packet[i] = 0xFF;
+            }
+
+            for (int i = 1; i <= 16; i++)
+            {
+                for (int k = 0; k < 6; k++)
+                {
+                    packet[i * 6 + k] = mac[k];
+                }
+            }
+
+            //Senden WOL
+            Client.Send(packet, packet.Length);
+
+        }
+
+        public byte[] GetMacArray(string mac)
+        {
+            if (string.IsNullOrEmpty(mac)) throw new ArgumentNullException("mac");
+            byte[] ret = new byte[6];
+            try
+            {
+                string[] tmp = mac.Split(':', '-');
+                if (tmp.Length != 6)
+                {
+                    //tmp = mac.Split('.');
+                    if (tmp.Length == 3)
+                    {
+                        for (int i = 0; i < 3; i++)
+                        {
+                            ret[i * 2] = byte.Parse(tmp[i].Substring(0, 2), System.Globalization.NumberStyles.HexNumber);
+                            ret[i * 2 + 1] = byte.Parse(tmp[i].Substring(2, 2), System.Globalization.NumberStyles.HexNumber);
+                        }
+                    }
+                    else
+                        for (int i = 0; i < 12; i += 2)
+                            ret[i / 2] = byte.Parse(mac.Substring(i, 2), System.Globalization.NumberStyles.HexNumber);
+                }
+                else
+                    for (int i = 0; i < 6; i++)
+                        ret[i] = byte.Parse(tmp[i], System.Globalization.NumberStyles.HexNumber);
+            }
+            catch
+            {
+                throw new ArgumentException("Argument doesn't have the correct format: " + mac, "mac");
+            }
+            return ret;
+        }
+
+
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace TurnOn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Btn_Starten = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(203, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "MAC Adresse";
+            // 
+            // Btn_Starten
+            // 
+            this.Btn_Starten.Location = new System.Drawing.Point(16, 90);
+            this.Btn_Starten.Name = "Btn_Starten";
+            this.Btn_Starten.Size = new System.Drawing.Size(95, 23);
+            this.Btn_Starten.TabIndex = 2;
+            this.Btn_Starten.Text = "Starten";
+            this.Btn_Starten.UseVisualStyleBackColor = true;
+            this.Btn_Starten.Click += new System.EventHandler(this.Btn_Starten_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 252);
+            this.Controls.Add(this.Btn_Starten);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Btn_Starten;
+    }
+}
+
