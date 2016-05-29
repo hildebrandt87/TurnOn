4515e5d09ad59f29f9539a49bd78fab05e16116f
Kai-Desk <Hildebrandt.Kai87@googlemail.com>
2016-05-29 22:35:36 +0200

    form überarbeitet

--- CReg.cs
+++ CReg.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Win32;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,141 @@ namespace TurnOn
 {
     class CReg
     {
+        RegistryKey Key;
+
+        //Daten in Registry Speichern
+        public bool Set_Registry(string Servername, string Username, string Password,string hex)
+        {
+            try
+            {
+                Key = Registry.CurrentUser.CreateSubKey("TurnOnSettings");
+
+                //Werte setzen
+                Key.SetValue("Servername", Servername);
+                Key.SetValue("Username", Username);
+                Key.SetValue("Password", Password);
+                Key.SetValue("Hex", hex);
+
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+
+
+            Key.Close();
+            return true;
+
+        }
+
+        //DIese Methode kann eigentlich gelöscht werden
+        public static string[] GetAll_RegistryKeys()
+        {
+            string[] Daten = new string[3];
+
+            try
+            {
+                RegistryKey BaseKey = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Default);
+                RegistryKey Key = BaseKey.OpenSubKey("TurnOnSettings");
+
+                //Werte holen
+                Daten[0] = Key.GetValue("Servername").ToString();
+                Daten[1] = Key.GetValue("Username").ToString();
+                Daten[2] = Key.GetValue("Password").ToString();
+
+            }
+            catch (Exception)
+            {
+ 
+            }
+
+            return Daten;
+        }
+
+        //Auslesen des Servername
+        public static string Get_Servername()
+        {
+            string name = null;
+
+            try
+            {
+                RegistryKey BaseKey = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Default);
+                RegistryKey Key = BaseKey.OpenSubKey("TurnOnSettings");
+
+                //Werte holen
+                name = Key.GetValue("Servername").ToString();
+
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.ToString());
+            }
+            return name;
+        }
+
+        //Auslesen des Username
+        public static string Get_Username()
+        {
+            string name = null;
+
+            try
+            {
+                RegistryKey BaseKey = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Default);
+                RegistryKey Key = BaseKey.OpenSubKey("TurnOnSettings");
+
+                //Werte holen
+                name = Key.GetValue("Username").ToString();
+
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.ToString());
+            }
+            return name;
+        }
+
+        //Auslesen des Passwords
+        public static string Get_Password()
+        {
+            string name = null;
+
+            try
+            {
+                RegistryKey BaseKey = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Default);
+                RegistryKey Key = BaseKey.OpenSubKey("TurnOnSettings");
+
+                //Werte holen
+                name = Key.GetValue("Password").ToString();
+
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.ToString());
+            }
+            return name;
+        }
+
+        //Hex Adresse holen
+        public static string Get_Hex()
+        {
+            string name = null;
+
+            try
+            {
+                RegistryKey BaseKey = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Default);
+                RegistryKey Key = BaseKey.OpenSubKey("TurnOnSettings");
+
+                //Werte holen
+                name = Key.GetValue("Hex").ToString();
+
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.ToString());
+            }
+            return name;
+        }
+
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -7,39 +7,150 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Management;
+using System.Timers;
+using System.Net;
+using System.Threading;
 
 namespace TurnOn
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+
+            //Eingestellter Server
+            txb_EingestelltServer.Text = CReg.Get_Servername();
+            tbx_ServerName.Text = CReg.Get_Servername();
+
+            //username einlesen
+            tbx_UserName.Text = CReg.Get_Username();
+
+            //Password einlesen
+            tbx_ServerPW.Text = CReg.Get_Password();
+
+            //Hex einlesen
+            tbx_Hexdata.Text = CReg.Get_Hex();
+
+
+            //////////////////////////////////////////////////
+            ////////////////TIMER////////////////////////////
+
+            System.Timers.Timer aTimer = new System.Timers.Timer(1000);
+            aTimer.Elapsed += aTimer_Elapsed;
+            aTimer.Enabled = true;
+
+            //war ein test wird nicht benötigt
+            //this.backgroundWorker1.RunWorkerAsync();
+
+        }
+
+
+
+        async void aTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            try
+            {
+                IPHostEntry hostName = Dns.GetHostByName(CReg.Get_Servername());
+
+                //InvokeRequired dient nur als Abfrage damit der Thread Save ausgeführt werden kann !
+                //Sehr wichtig da eigentlich kein fremder thread auf den thread zugreifen kann welcher die steurelemente erstellt hat
+                if (InvokeRequired)
+                {
+                    Invoke((Func<bool>)WriteStatusOnline);
+                }
+            }
+            catch (Exception)
+            {
+                if (InvokeRequired)
+                {
+                    //Methoden aufruf mit einem Invoke
+                    Invoke((Func<bool>)WriteStatusOffline);
+                }
+            }
+
+
+
+        }
+
+        //Methoden müssen auserhalb deklariert werden da sonst kein thread save aufruf nicht klappt
+        bool WriteStatusOnline()
+        {
+            Status_front.Text = ">> Rechner ist online <<";
+            Status_front.BackColor = System.Drawing.Color.Green;
+            return true;
+        }
+        bool WriteStatusOffline()
+        {
+            Status_front.Text = ">> Rechner ist offline <<";
+            Status_front.BackColor = System.Drawing.Color.Red;
+            return false;
         }
 
         private void Btn_Starten_Click(object sender, EventArgs e)
         {
-            String macStr;
             byte[] macByte;
             byte[] ret = new byte[6];
 
-            //String aus Textbox holen
-            macStr = textBox1.Text;
+            //Wenn alles ok ist bitte wieder löschen
+            //macStr = "9C-B6-54-A9-DE-FA";
 
-            //TO Do: MAC IN REG Abspeichern um eingabe noch zu wiederholen
 
             //Klasseninstanz eröffnen
             CWol Up = new CWol();
 
-            //EIGENBAU DES STRINGS UMWANDELN !!
-            //String umwandeln
-            macByte= Up.GetMacArray(macStr);
+            //Hex String umwandeln
+            macByte = Up.GetMacArray(CReg.Get_Hex());
 
             //Übergabe 
             Up.WakeOnLan(macByte);
-            
+
             //Klasse beenden
             Up = null;
+
+        }
+
+
+        private void btn_shutdown_Click(object sender, EventArgs e)
+        {
+            CShutDown End = new CShutDown();
+            try
+            {
+                End.ShutDown(CReg.Get_Servername(), CReg.Get_Username(), CReg.Get_Password());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Fehler bei Methode: shutdown." + ex.ToString());
+            }
+
+            End = null;
+        }
+
+        private void Btn_SaveReg_Click(object sender, EventArgs e)
+        {
+            //Daten aus Textboxen auslesen
+            string local_servername = tbx_ServerName.Text.ToString();
+            string local_username = tbx_UserName.Text.ToString();
+            string local_password = tbx_ServerPW.Text.ToString();
+            string local_hex = tbx_Hexdata.Text.ToString();
+
+            
+            CReg SaveSettings = new CReg();
+
+            //Prüfen ob Speichern erfolgreich ist
+            if (SaveSettings.Set_Registry(local_servername, local_username, local_password, local_hex) == true)
+                toolStripStatusLabel1.Text = "Speichern erfolgreich";
+            else
+                toolStripStatusLabel1.Text = "Beim Speichern ist ein Fehler aufgetreten";
+
+            SaveSettings = null;
+        }
+
+        //wird nicht gestartet daher kann diese methode vernachlässigt werden
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
 
         }
 
