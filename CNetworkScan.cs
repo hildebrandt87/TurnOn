d5ba183c4bb9050d8c43b2346c0d9c9a2bfd7ce5
hildebrandt87 <hildebrandt.kai87@googlemail.com>
2016-11-06 19:03:36 +0100

    networkscan cs

---
+++ CNetworkScan.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurnOn
+{
+    class CNetworkScan
+    {
+        public void SearchOnline()
+        {
+            // Alle PCs im Netzwerk auflisten
+            System.DirectoryServices.DirectoryEntry root = new System.DirectoryServices.DirectoryEntry("WinNT:");
+            foreach (System.DirectoryServices.DirectoryEntry dDom in root.Children)
+            {
+                foreach (System.DirectoryServices.DirectoryEntry dPC in dDom.Children)
+                {
+                    MessageBox.Show(dDom.Name + " / " + dPC.Name);
+                }
+            }
+        }
+
+    
+    }
+}
